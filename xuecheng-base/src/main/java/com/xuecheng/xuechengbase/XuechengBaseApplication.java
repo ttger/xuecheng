b3f9d33d6c4681d789e2d1dd696327ada1f5d056
zhwtiger <759908565@qq.com>
2023-03-06 12:03:05 +0800

    工程创建

---
+++ xuecheng-base/src/main/java/com/xuecheng/xuechengbase/XuechengBaseApplication.java
@@ -0,0 +1,13 @@
+package com.xuecheng.xuechengbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XuechengBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XuechengBaseApplication.class, args);
+    }
+
+}
---
+++ xuecheng-base/src/test/java/com/xuecheng/xuechengbase/XuechengBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xuecheng.xuechengbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XuechengBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

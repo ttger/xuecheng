8d89061ef4452c49a7c8875c078b5cc5daeb3536
zhwtiger <759908565@qq.com>
2023-03-09 09:43:21 +0800

    system模块

---
+++ xuecheng-base/src/main/java/com/xuecheng/xuechengbase/constants/Constants.java
@@ -0,0 +1,12 @@
+package com.xuecheng.xuechengbase.constants;
+
+/**
+ * 常量配置
+ */
+public class Constants {
+
+    public static final String CODE_200 = "200";
+    public static final String CODE_500 = "500";
+    public static final String CODE_400 = "400";
+    public static final String CODE_300 = "300";
+}
---
+++ xuecheng-base/src/main/java/com/xuecheng/xuechengbase/untils/CommonResult.java
@@ -0,0 +1,32 @@
+package com.xuecheng.xuechengbase.untils;
+
+import com.xuecheng.xuechengbase.constants.Constants;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 前后端接口联调，通用返回类
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CommonResult {
+    private String code;
+    private String msg;
+    private Object data;
+
+    public static CommonResult success(){
+        return new CommonResult(Constants.CODE_200,"操作成功",null);
+    }
+
+    public static CommonResult success(Object data){
+        return new CommonResult(Constants.CODE_200,"操作成功",data);
+    }
+
+    public static CommonResult error(){
+        return new CommonResult(Constants.CODE_500,"系统错误",null);
+    }
+
+
+}
